33938db0cab8c6de2fb573caef1db9f557bdee60
Boris Berman <boris@berman.solutions>
2022-06-25 20:13:48 +0300

    Copy udev rule

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt install build-essential dkms
 
 mkdir /usr/src/r8152-2.16.1/
 cp -rv * /usr/src/r8152-2.16.1/
+cp /usr/src/r8152-2.16.1/50-usb-realtek-net.rules /lib/udev/rules.d/
 
 sudo dkms add -m r8152 -v 2.16.1
 sudo dkms build -m r8152 -v 2.16.1
